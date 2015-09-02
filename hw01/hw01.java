622afc66d236129f92b1037204ecfd70e805856c
sjf318 <sjf318@lehigh.edu>
2015-09-02 16:11:44 +0000

    new version with the github stuff

---
+++ hw01/hw01.java
@@ -0,0 +1,28 @@
+//Sam Forman
+// HW 1
+//hw01
+//
+// first compile the program
+//     javac hw01.java
+// run the program
+//     java hw01.java//
+
+// define a class
+public class hw01{
+    
+// add main method
+  public static void main(String[] args) {
+        System.out.println("     -----------");
+        System.out.println("     | WELCOME |");
+        System.out.println("     -----------");
+   System.out.println("  ^  ^  ^  ^  ^  ^");
+  System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+  System.out.println("|-S--J--F--3--1--8-|");
+  System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+   System.out.println("  v  v  v  v  v  v");
+  System.out.println("Over the summer I hiked Mount Washington. The weather was extemely unpredictable, but overall it was an incredible hike.")
+  ;
+  
+   //test/testtest
+  }
+}
